e8e269cd2c34ab9ddfee992e97ef141bda464ba1
Moataz <moataz.elbayaa@stud.hshl.de>
2024-02-06 02:26:58 +0100

    <>

---
+++ Application_Code_and_Implentation/project_1/project_1.cache/ip/2023.1/c/8/c8e4db9abd2b0696/pmod_design_1_clk_wiz_1_0_stub.v
@@ -0,0 +1,25 @@
+// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+// Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2023.1 (win64) Build 3865809 Sun May  7 15:05:29 MDT 2023
+// Date        : Tue Jan 16 03:41:05 2024
+// Host        : Moataz running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+//               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ pmod_design_1_clk_wiz_1_0_stub.v
+// Design      : pmod_design_1_clk_wiz_1_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a100tcsg324-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix(clk_out1, reset, locked, clk_in1_p, clk_in1_n)
+/* synthesis syn_black_box black_box_pad_pin="reset,locked,clk_in1_p,clk_in1_n" */
+/* synthesis syn_force_seq_prim="clk_out1" */;
+  output clk_out1 /* synthesis syn_isclock = 1 */;
+  input reset;
+  output locked;
+  input clk_in1_p;
+  input clk_in1_n;
+endmodule
